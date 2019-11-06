7fa8a695c7372fa96ad0b9c3a5d2b2d08f8cce0b
Angel Joshua Tienda Arellano <56735846+joshua004@users.noreply.github.com>
2019-11-06 14:31:43 -0600

    Update mcd.c

--- mcd.c
+++ mcd.c
@@ -1,7 +1,28 @@
-/**mcd.c*/
+#include <stdio.h>
+#include <stdlib.h>
 
-int mcd(int N,int D)
+int mcd(int n,int d)
 {
- return 1; 
+  int N,D,R;
+  N=abs(n);D=abs(d);
+  while((R=D%N)!=0){
+#ifdef PRELIMINAR
+  printf("%d = %d(%d)+%d\n",D,D/N,N,R);
+#endif
+    D=N;
+    N=R;
+  }
+#ifdef PRELIMINAR
+  printf("%d = %d(%d)+%d\n",D,D/N,N,R);
+#endif
+  return N;
 }
 
+/**Se trata de simplificar las fracciones
+ */
+void simplificar(int frac[2])
+{
+  int intMCD=mcd(frac[0],frac[1]);
+  frac[0]=frac[0]/intMCD;
+  frac[1]=frac[1]/intMCD;
+}
