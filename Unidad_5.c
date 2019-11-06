953916a246893547654db2ce8eee390e6224cc42
IZKls1 <56328587+IZKls1@users.noreply.github.com>
2019-11-06 13:38:03 -0600

    Update Unidad_5.c

--- Unidad_5.c
+++ Unidad_5.c
@@ -6,9 +6,16 @@ int mcd(int,int);
 int
 main(int argc,char *argv[])
 {
-  struct Racional R1={112,8,mcd};
+  struct Racional R1={1,20,mcd};
   int MCD=R1.f_Pt(R1.num,R1.den);
-  printf("%d/%d=%d/%d",R1.num,R1.den,R1.num/MCD,R1.den/MCD);
+  printf("%d/%d=%d/%d\n",R1.num,R1.den,R1.num/MCD,R1.den/MCD);
+ int i,j;
+ for(i=1;i<=20;i++){
+         for(j=20;j<=40;j++){
+                 R1.num=i;R1.den=j;
+        MCD=R1.f_Pt(R1.num,R1.den);
+        printf("%d/%d=%d/%d\n",R1.num,R1.den,R1.num/MCD,R1.den/MCD);
+         }
+ }
   return 0;
 }/*end main()*/
-
