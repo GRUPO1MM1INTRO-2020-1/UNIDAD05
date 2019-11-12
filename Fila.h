03e06250ac7e01a8c6955697d6c67711d5a1a7de
U-DESKTOP-9EAUVS9\LMC <lamberto9@gmail.com>
2019-11-12 13:25:40 -0600

    2019.11.12.13.22 Se coloc\'o argumento struct Fila * al apuntador a funcion de la struct Fila

--- Fila.h
+++ Fila.h
@@ -5,7 +5,7 @@
 struct Fila{
   int n;              /*longitud de la fila*/
   struct Racional *R_Pt;/*apunta a n Racionales*/
-  void (*mult)(struct Fila *,struct Racional *);/*multiplcar la fila por unn Racional*/
+  void (*mult)(struct Fila *,struct Racional *);/*multiplcar la fila por un Racional*/
   void (*print)(struct Fila *);      /*imprime la fila*/
 };/*end struct Fila*/
 #endif /*FILA_H*/
