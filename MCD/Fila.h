7f23487bc3f229ef64d0d5f2dbd19d232d5268ed
JRodolfoL <56652116+JRodolfoL@users.noreply.github.com>
2019-11-12 14:20:00 -0600

    Add files via upload

---
+++ MCD/Fila.h
@@ -0,0 +1,11 @@
+/**Fila.h*/
+#ifndef FILA_H
+#define FILA_H
+#include "Racional.h"
+struct Fila{
+  int n;              /*longitud de la fila*/
+  struct Racional *R_Pt;/*apunta a n Racionales*/
+  void (*mult)(struct Fila *,struct Racional *);/*multiplcar la fila por un Racional*/
+  void (*print)(struct Fila *);      /*imprime la fila*/
+};/*end struct Fila*/
+#endif /*FILA_H*/
