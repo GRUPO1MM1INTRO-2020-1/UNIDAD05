620a42fe9869b52c9b34606b5201134d35b23bd5
usuario <usuario@pc-88-193.dinamico.upiita.ipn.mx>
2019-11-22 11:29:31 -0600

    2019.11.22.11.30

---
+++ Minicript/alphabet.c
@@ -0,0 +1,11 @@
+/** alphabet.c 
+ *  Contiene  el arreglo de caracteres alpha */
+
+char alpha[][6]={
+  {' ','a','e','i','o','u'},
+  {'a','e','i','o','u',' '},
+  {'e','i','o','u',' ','a'},
+  {'i','o','u',' ','a','e'},
+  {'o','u',' ','a'.'e','i'},
+  {'u',' ','a','e','i','o'}
+};
